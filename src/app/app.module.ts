2f42a6476531f3d2431e8dbcc3e2046747b09b7e
sukumar <sukku183msd@gmail.com>
2018-06-14 14:44:08 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,18 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AddDataPage } from '../pages/add-data/add-data';
+import { EditDataPage } from '../pages/edit-data/edit-data';
+
+import { SQLite } from '@ionic-native/sqlite';
+import { Toast } from '@ionic-native/toast';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    EditDataPage,
+    AddDataPage
   ],
   imports: [
     BrowserModule,
@@ -19,12 +26,16 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    EditDataPage,
+    AddDataPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    SQLite,
+    Toast
   ]
 })
-export class AppModule {}
+export class AppModule { }
